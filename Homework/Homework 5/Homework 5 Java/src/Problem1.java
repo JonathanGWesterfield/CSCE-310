eef6f1d52f39d10b67e9728c3d92d01b02ddf86c
Jonathan G. Westerfield <jgwesterfield@gmail.com>
2019-02-22 11:17:38 -0600

    Started Homework 5

---
+++ Homework/Homework 5/Homework 5 Java/src/Problem1.java
@@ -0,0 +1,119 @@
+import java.sql.*;
+import java.util.*;
+import java.io.*;
+
+
+public class Problem1
+{
+    private String connString = "jdbc:mariadb://localhost:3306/Computers?user=root&password=root";
+    Connection conn;
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            Problem1 problem1 = new Problem1();
+            problem1.findByPrice();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Problem1() throws SQLException
+    {
+        this.conn = DriverManager.getConnection(connString);
+        System.out.println("Connected to Database");
+    }
+
+    public Double getUserPrice()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input a Price of the computer you are looking for: ");
+        return Double.parseDouble(scanner.next());
+    }
+
+    public String getCorrectModel(double userPrice, ArrayList<String> model, ArrayList<Double> price)
+    {
+        int closestMatchIndex = 0;
+        double bestPriceDiff = Double.MAX_VALUE;
+        double priceDiff;
+        if(model.size() != price.size())
+        {
+            System.out.println("ERROR: Models and Prices did not return the same number of results");
+            System.exit(-1);
+        }
+        for(int i = 0; i < price.size(); i++)
+        {
+            priceDiff = Math.abs(userPrice - price.get(i));
+            if (priceDiff < bestPriceDiff)
+            {
+                bestPriceDiff = priceDiff;
+                closestMatchIndex = i;
+            }
+        }
+
+        return model.get(closestMatchIndex);
+    }
+
+    /**
+     * Returns the pc Model to get the information for
+     * @return
+     * @throws SQLException
+     */
+    public String execPriceStmt() throws SQLException
+    {
+        Double userPrice = getUserPrice();
+        String stmt = "SELECT model, price FROM PC WHERE price > " + (userPrice - 5) +
+                "AND price < " + (userPrice + 5) + ";";
+        Statement getPC = conn.createStatement();
+        ResultSet result = getPC.executeQuery(stmt);
+
+        // Parallel arrays for storing pcModels and prices
+        ArrayList<String> pcModels = new ArrayList<>();
+        ArrayList<Double> pcPrices = new ArrayList<>();
+
+        System.out.println();
+        while (result.next())
+        {
+            String model = result.getString("model");
+            pcModels.add(model);
+
+            double price = result.getDouble("price");
+            pcPrices.add(price);
+
+            System.out.printf("%s\t%f\n", model, price);
+        }
+
+        String priceModelMatch = getCorrectModel(userPrice, pcModels, pcPrices);
+        System.out.println("Closest price match model is: " + priceModelMatch);
+
+        return priceModelMatch;
+    }
+
+    public void findByPrice() throws SQLException
+    {
+        String model = execPriceStmt();
+        String query = "SELECT maker, model FROM Product WHERE model='" + model + "' UNION " +
+                "SELECT speed FROM PC WHERE model='" + model + "';";
+        Statement getPCInfo = conn.createStatement();
+        ResultSet result = getPCInfo.executeQuery(query);
+
+        String resultMaker, resultModel, resultSpeed;
+        resultMaker = resultModel = resultSpeed = "";
+
+        while (result.next())
+        {
+            resultMaker = result.getString("maker");
+            resultModel = result.getString("model");
+            resultSpeed = result.getString("speed");
+        }
+
+        System.out.printf("Closest match is: %s, %s, %s\n", resultMaker, resultModel, resultSpeed);
+    }
+
+
+}
