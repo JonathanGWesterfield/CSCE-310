328e31bda54bd40ab1ae2e6fb4f431311d32acca
“JonathanGWesterfield” <jgwesterfield@gmail.com>
2019-02-25 01:04:37 -0600

    Working on problem 2

--- Homework/Homework 5/Homework 5 Java(1)/src/Problem2.java
+++ Homework/Homework 5/Homework 5 Java(1)/src/Problem2.java
@@ -9,12 +9,17 @@ public class Problem2
 
     private String connString = "jdbc:mariadb://localhost:3306/Computers?user=root&password=root";
     Connection conn;
+    private ShipClassObj newClass;
+    private ArrayList<ShipsObj> newShipslist;
 
     public static void main(String[] args)
     {
         try
         {
             Problem2 pr = new Problem2();
+            pr.getClassInfo();
+            // pr.getNewShipsList();
+            pr.insertClass();
 
             pr.closeConn();
         }
@@ -50,15 +55,68 @@ public class Problem2
     {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Information for a new Class of Ship");
         System.out.print("What is the class of ship: ");
         String shipClass = scanner.next();
 
         System.out.print("What is the type of the ship class: ");
         String type = scanner.next();
 
-        System.out.println("What is the country? ");
+        System.out.print("What is the country? ");
         String country = scanner.next();
 
+        System.out.print("How many guns for this class? ");
+        int numGuns = Integer.parseInt(scanner.next());
 
+        System.out.print("What is the bore of the guns? ");
+        int bore = Integer.parseInt(scanner.next());
+
+        System.out.print("What is the displacement of this class? ");
+        int disp = Integer.parseInt(scanner.next());
+
+        this.newClass = new ShipClassObj(shipClass, type, country, numGuns, bore, disp);
+    }
+
+    public void getNewShipsList()
+    {
+        boolean done = false;
+        Scanner scanner = new Scanner(System.in);
+        this.newShipslist = new ArrayList<>();
+
+        System.out.println("Input information for new Ships of the new class");
+        while (!done)
+        {
+            System.out.print("Enter the ship name: ");
+            String name = scanner.next();
+
+            System.out.print("Enter launch date: ");
+            int launched = Integer.parseInt(scanner.next());
+
+            newShipslist.add(new ShipsObj(name, this.newClass.getShipClass(), launched));
+
+            System.out.print("Done entering ships? (y/n): ");
+            if(scanner.next().equalsIgnoreCase("y"))
+                done = true;
+        }
+    }
+
+    public void insertClass() throws SQLException
+    {
+        String query = "INSERT INTO classes (class, type, country, numGuns, bore, disp) VALUES ('?', '?', '?', ?, ?, ?);";
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, newClass.getShipClass());
+        stmt.setString(2, newClass.getType());
+        stmt.setString(3, newClass.getCountry());
+        stmt.setInt(4, newClass.getNumGuns());
+        stmt.setInt(5, newClass.getBore());
+        stmt.setInt(6, newClass.getDisp());
+
+        int i = stmt.executeUpdate();
+        System.out.println(i + " records inserted into classes table");
+    }
+
+    public void insertShipList()
+    {
+        String query = "INSERT INTO ships VALUES ('?', '?', ?);";
     }
 }
