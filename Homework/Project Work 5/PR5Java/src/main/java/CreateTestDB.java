8c4de4ac107fd3b3e6a6a670bea15f9d2a3456cd
ecen489 <jgwesterfield@gmail.com>
2019-03-16 20:50:39 -0500

    Added more to the Project work 5

---
+++ Homework/Project Work 5/PR5Java/src/main/java/CreateTestDB.java
@@ -0,0 +1,61 @@
+
+import java.util.Arrays;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+//http://mongodb.github.io/mongo-java-driver/3.6/driver/getting-started/quick-start/
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.*;
+
+public class CreateTestDB {
+
+	public static void main(String args[]) {
+		MongoClient mongoClient = null;
+		try {
+			mongoClient = mongoClient = MongoClients.create(
+					MongoClientSettings.builder()
+							.applyToClusterSettings(builder ->
+									builder.hosts(Arrays.asList(new ServerAddress("localhost", 27017))))
+							.build());
+			MongoCollection<Document> coll = null;
+			MongoDatabase db = mongoClient.getDatabase("testdb");
+			System.out.println("Database Connection Successful!");
+			MongoCollection<Document> collec = db.getCollection("dbcollec");
+			Document doc = new Document("name", "MongoDB").append("type", "database").append("count", 1)
+					.append("versions", Arrays.asList("v4.0", "v3.6", "v2.6"))
+					.append("info", new Document("x", 203).append("y", 102));
+			collec.insertOne(doc);
+			System.out.println("Document is inserted successfully");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			if (mongoClient != null)
+				mongoClient.close();
+		}
+
+	}
+}
+/**
+ * db.profiles.find({name:"Johnny Manziel"},{player_id:1, name:1,
+ * _id:0}).pretty()
+ * 
+ * db.games.find({player_id:2240}).count()
+ * 
+ * db.games.aggregate([{$match:{player_id:3950}},{"$group" : {_id:"$player_id",
+ * count:{$sum:1}}}])
+ * 
+ * db.games.aggregate([{$match:{player_id:2648}},{"$group" :
+ * {_id:"$player_id",BrownpTDs:{$sum:"$receiving_touchdowns"
+ * },BrownrTDs:{$sum:"$rushing_touchdowns" }}},{"$project" : {_id:"$player_id",
+ * BrownTDs:{$sum:["$BrownpTDs","$BrownrTDs"] }}}])
+ * 
+ * db.games.aggregate([{$match:{player_id:4847}},{$group:{_id:"$year",count:{$sum:1}}}])
+ * 
+ * db.games.aggregate([{$match:{year:"1990", team: "SEA",
+ * defense_interceptions:{$ne:0}}}, {"$project":{_id:0, player:"$player_id",
+ * gameNum:"$game_number"}}])
+ */
---
+++ Homework/Project Work 5/PR5Java/src/main/java/Problem2A.java
@@ -0,0 +1,48 @@
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public class Problem2A
+{
+    //@SuppressWarnings("deprecation")
+    public static void main(String[] args) throws Exception {
+        MongoClient mongoClient = null;
+        try {
+            mongoClient = MongoClients.create(
+                    MongoClientSettings.builder()
+                            .applyToClusterSettings(builder ->
+                                    builder.hosts(Arrays.asList(new ServerAddress("localhost", 27017))))
+                            .build()
+            )/*new MongoClient( "localhost" , 27017 )*/;
+            MongoCollection<Document> coll = null;
+            MongoDatabase database = mongoClient.getDatabase("csce310");
+            if (database != null) {
+
+                System.out.println("Connect to Database Successful");
+                coll = database.getCollection("games");
+                if (coll != null)
+                    System.out.println("Select Collection Successful");
+                else System.out.println("Collection NOT found!");
+            } else System.out.println("Database NOT found!");
+
+            // TODO: FIGURE OUT THE MONGODB QUERY
+
+            Document myDoc = coll.find().first(); /** ITS THIS LINE */
+            if (myDoc != null)
+                System.out.println(myDoc.toJson());
+            else System.out.println("First document NOT found");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mongoClient != null)
+                mongoClient.close();
+        }
+    }
+}
